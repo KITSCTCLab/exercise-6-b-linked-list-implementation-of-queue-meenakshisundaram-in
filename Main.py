0d3d7eb31a557f7d9002147c6b372a37bb027401
meenakshisundaram-in <109644642+meenakshisundaram-in@users.noreply.github.com>
2022-09-27 21:35:09 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 class Node:
-    def _init_(self, data):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
 
 class Queue:
-    def _init_(self):
+    def __init__(self):
         self.head = None
         self.last = None
 
